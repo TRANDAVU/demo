17bc22b7c14a81004ac088adb7e33884ae569edb
Trần Hoài Dạ Vũ <52185531+TRANDAVU@users.noreply.github.com>
2023-09-13 14:44:04 +0700

    Update manual.tsx

--- src/pages/components/manual.tsx
+++ src/pages/components/manual.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-
+import Link from 'next/link'
 function Manual() {
     return (
         <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-            <a
-                href="/users/about"
+            <Link href="/users/about"
                 className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
                 rel="noopener noreferrer"
             >
@@ -17,9 +16,10 @@ function Manual() {
                 <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
                     Giới thiệu về Next.
                 </p>
-            </a>
+            </Link>
 
-            <a
+
+            <Link
                 href="/users/folder"
                 className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
                 rel="noopener noreferrer"
@@ -33,9 +33,8 @@ function Manual() {
                 <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
                     Folder Next
                 </p>
-            </a>
-
-            <a
+            </Link>
+            <Link
                 href="/users/template"
                 className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
                 rel="noopener noreferrer"
@@ -49,7 +48,7 @@ function Manual() {
                 <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
                     Cách dùng css và các thành phần bên ngoài.
                 </p>
-            </a>
+            </Link>
 
             <a
                 href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
