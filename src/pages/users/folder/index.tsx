fced40c9fc5f88949b6bcfad77d2143b4236bf9f
Trần Hoài Dạ Vũ <52185531+TRANDAVU@users.noreply.github.com>
2023-09-13 15:10:52 +0700

    .

--- src/pages/users/folder/index.tsx
+++ src/pages/users/folder/index.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import Head from 'next/head'
 import Title from '@/pages/components/title'
 import Manual from '@/pages/components/manual'
 const inter = Inter({ subsets: ['latin'] })
@@ -8,22 +7,44 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Folder() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-between p-24 `}
     >
-      <Title title={'Folder'}/>
+      <Title title={'Folder'} />
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="content-container">
+        <div className="content">
+          <h1><center>Cấu trúc folder Next</center></h1>
+          <div>
+            <Image
+              src="/images/I3.png"
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+          </div>
+
+          <div>
+            <b>-public</b>: chứa các hình ảnh và js(nếu có) của dự án. <br />
+            <b>-components</b>:Lưu các thành phần có thể dùng lại ở tất cả các trang header,footer... <br />
+            <b>-users(custom)</b>:Folder lưu các thư mục con tương ứng với root của các trang con bên trong<br />
+            <b>-users {'->'} about(custom)</b>:Trang con trong folder có thể tạo các component dùng cho trang con<br />
+            <b>_app.tsx</b>:Khai báo css custom<br />
+            <Image
+              src="/images/I4.png"
+              width={500}
+              height={500}
+              alt="Picture of the author"
+            />
+          </div>
+
+
+        </div>
       </div>
 
-      <Manual/>
+
+
+      <Manual />
+
     </main>
   )
 }
